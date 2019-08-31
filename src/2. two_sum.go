cfebd0bd42b3bfc5b5d531b27cbff1c7bab783be
dengbin15 <1397773247@qq.com>
2019-08-31 21:22:55 +0800

    rename

--- src/2. two_sum.go
+++ src/2. two_sum.go
@@ -1,7 +1,7 @@
 package src
 
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
 
@@ -10,37 +10,35 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	headNode := node
 	flag := 0
 
-	for node1 , node2 := l1,l2 ; node1.Next != nil || node2.Next != nil ; {
+	for node1, node2 := l1, l2; node1.Next != nil || node2.Next != nil; {
 		if node1.Next == nil {
 			node1.Next = &ListNode{}
-		}else if node2.Next == nil{
+		} else if node2.Next == nil {
 			node2.Next = &ListNode{}
 		}
 		node1 = node1.Next
 		node2 = node2.Next
 	}
 
-	for ;l1 != nil && l2 != nil; {
-		if l1.Val + l2.Val + flag >=10 {    //相加大于10
-			node.Val = l1.Val + l2.Val + flag -10
+	for l1 != nil && l2 != nil {
+		if l1.Val+l2.Val+flag >= 10 { //相加大于10
+			node.Val = l1.Val + l2.Val + flag - 10
 			flag = 1
 			if l1.Next == nil && l2.Next == nil {
-				node.Next = &ListNode{1,nil}
+				node.Next = &ListNode{1, nil}
 			}
-		}else{
-			node.Val = l1.Val + l2.Val + flag   //相加小于10
+		} else {
+			node.Val = l1.Val + l2.Val + flag //相加小于10
 			flag = 0
 		}
-		if l1.Next != nil && l2.Next != nil{
+		if l1.Next != nil && l2.Next != nil {
 			l1 = l1.Next
 			l2 = l2.Next
 			node.Next = &ListNode{}
 			node = node.Next
-		}else{
+		} else {
 			break
 		}
 	}
 	return headNode
 }
-
-
